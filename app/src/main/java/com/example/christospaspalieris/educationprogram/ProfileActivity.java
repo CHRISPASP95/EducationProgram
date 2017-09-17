633e54a66acef9ed31ccb42d1056b878c6a773b5
peira <stathiskaps@gmail.com>
2017-09-17 17:06:51 +0300

    XRHSTO PAIKTARA POULA MAS TH MPALA

--- app/src/main/java/com/example/christospaspalieris/educationprogram/ProfileActivity.java
+++ app/src/main/java/com/example/christospaspalieris/educationprogram/ProfileActivity.java
@@ -1,12 +1,15 @@
 package com.example.christospaspalieris.educationprogram;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,6 +22,7 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
@@ -29,6 +33,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity  {
 
@@ -40,7 +48,8 @@ public class ProfileActivity extends AppCompatActivity  {
 
 
     private FirebaseAuth mAuth;
-    private DatabaseReference dbUserReference;
+    private DatabaseReference mDatabaseReference;
+    private StorageReference mStorageReference;
     private FirebaseUser user;
 
     private TextView profile_label;
@@ -61,9 +70,15 @@ public class ProfileActivity extends AppCompatActivity  {
     private Button canceledit;
 
     private boolean changedpass;
+    private boolean editmode;
     UserInformation userInformation;
 
     private ToggleButton changepass;
+
+    private ProgressDialog progressSave;
+
+    private Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,6 +86,8 @@ public class ProfileActivity extends AppCompatActivity  {
 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
+
+        progressSave =  new ProgressDialog(getApplicationContext());
 
         Typeface typeFace= Typeface.createFromAsset(getAssets(),"fonts/ComicBook.otf");
 
@@ -103,6 +120,8 @@ public class ProfileActivity extends AppCompatActivity  {
         newpass.setTypeface(typeFace);
         age.setTypeface(typeFace);
 
+
+
         changepass.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -121,34 +140,17 @@ public class ProfileActivity extends AppCompatActivity  {
             }
         });
 
-        dbUserReference = FirebaseDatabase.getInstance().getReference("USERS").child(user.getUid());
-        dbUserReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
 
-                userInformation = dataSnapshot.getValue(UserInformation.class);
-                firstName.setText(userInformation.getFirstName());
-                lastName.setText(userInformation.getLastName());
-                Age.setText(userInformation.getAge());
-                firstname.setHint(userInformation.getFirstName());
-                lastname.setHint(userInformation.getLastName());
-                age.setHint(userInformation.getAge());
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("Profile_images");
 
 
-
-
-            }
-
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("USERS").child(user.getUid());
+        LoadUserInfo();
 
         editprofile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                editmode = true;
                 firstName.setVisibility(View.GONE);
                 lastName.setVisibility(View.GONE);
                 Age.setVisibility(View.GONE);
@@ -166,6 +168,7 @@ public class ProfileActivity extends AppCompatActivity  {
         canceledit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                editmode = false;
                 firstName.setVisibility(View.VISIBLE);
                 lastName.setVisibility(View.VISIBLE);
                 Age.setVisibility(View.VISIBLE);
@@ -190,22 +193,47 @@ public class ProfileActivity extends AppCompatActivity  {
         saveprofile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //progressSave.setMessage("Saving");
+                //progressSave.show();
+                if(imageUri!=null) {
+                    mStorageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests") String imageUri = taskSnapshot.getDownloadUrl().toString();
+                            mDatabaseReference.child("image").setValue(imageUri);
+                            //progressSave.dismiss();
+                        }
+                    });
+                }
+
+                if(!TextUtils.isEmpty(firstname.getText().toString()))
+                {
+                    mDatabaseReference.child("first_name").setValue(firstname.getText().toString());
+                    Log.d(TAG,"Changed first name");
+                }
+                if(!TextUtils.isEmpty(lastname.getText().toString()))
+                {
+                    mDatabaseReference.child("last_name").setValue(lastname.getText().toString());
+                    Log.d(TAG,"Changed last name");
+                }
+                if(!TextUtils.isEmpty(age.getText().toString()))
+                {
+                    mDatabaseReference.child("age").setValue(age.getText().toString());
+                    Log.d(TAG,"Changed age");
+                }
+
                 if(changepass.isChecked())
                 {
-                    if(newpass.getText().length()<=6)
-                    {
-                        Toast.makeText(ProfileActivity.this,"Password needs to have at least 6 characters", Toast.LENGTH_SHORT).show();
+                    if(newpass.getText().length()<6) {
+                        Toast.makeText(ProfileActivity.this, "Password needs to have at least 6 characters", Toast.LENGTH_SHORT).show();
                         return;
                     }
-                    Log.d(TAG,userInformation.getPassword());
+
                     if(String.valueOf(oldpass.getText()).equals(userInformation.getPassword()))
                     {
                         AuthCredential credential = EmailAuthProvider
                                 .getCredential(userInformation.getEmail(), userInformation.getPassword());
 
-                        Log.d(TAG,userInformation.getEmail());
-                        Log.d(TAG,userInformation.getPassword());
-                        Log.d(TAG,String.valueOf(newpass.getText()));
 
                         // Prompt the user to re-provide their sign-in credentials
                         user.reauthenticate(credential)
@@ -213,16 +241,15 @@ public class ProfileActivity extends AppCompatActivity  {
                                     @Override
                                     public void onComplete(@NonNull Task<Void> task) {
                                         if (task.isSuccessful()) {
-                                            dbUserReference.child("password").setValue(String.valueOf(newpass.getText()));
-                                            Log.d(TAG,"elelelele");
-
                                             user.updatePassword(String.valueOf(newpass.getText())).addOnCompleteListener(new OnCompleteListener<Void>() {
                                                 @Override
                                                 public void onComplete(@NonNull Task<Void> task) {
                                                     if (task.isSuccessful()) {
-                                                        Log.d(TAG, "Password updated");
+                                                        mDatabaseReference.child("password").setValue(String.valueOf(newpass.getText()));
+                                                        Toast.makeText(ProfileActivity.this,"Your password was successfully changed", Toast.LENGTH_SHORT).show();
+                                                        Log.d(TAG,"Changed password");
                                                     } else {
-                                                        Log.d(TAG, "Error password not updated");
+                                                        Toast.makeText(ProfileActivity.this,"An error occurred while updating your password", Toast.LENGTH_SHORT).show();
                                                     }
                                                 }
                                             });
@@ -237,6 +264,9 @@ public class ProfileActivity extends AppCompatActivity  {
                 {
 
                 }
+                LoadUserInfo();
+
+                //progressSave.dismiss();
             }
         });
 
@@ -245,45 +275,84 @@ public class ProfileActivity extends AppCompatActivity  {
         profilephoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(ProfileActivity.this, profilephoto);
-                //Inflating the Popup using xml file
-                popup.getMenuInflater()
-                        .inflate(R.menu.menu_list, popup.getMenu());
+                if(editmode)
+                {
+                    PopupMenu popup = new PopupMenu(ProfileActivity.this, profilephoto);
+                    //Inflating the Popup using xml file
+                    popup.getMenuInflater()
+                            .inflate(R.menu.menu_list, popup.getMenu());
 
-                //registering popup with OnMenuItemClickListener
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        if(item.getTitle().equals("Browse"))
-                        {
-                            Intent galleryIntent = new Intent();
-                            galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-                            galleryIntent.setType("image/*");
-                            startActivityForResult(galleryIntent,GALLERY_REQUEST);
-                        }
-                        else if(item.getTitle().equals("Take a picture"))
-                        {
-                            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                            if (takePictureIntent.resolveActivity(ProfileActivity.this.getPackageManager()) != null) {
-                                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+                    //registering popup with OnMenuItemClickListener
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        public boolean onMenuItemClick(MenuItem item) {
+                            if (item.getTitle().equals("Browse")) {
+                                Intent galleryIntent = new Intent();
+                                galleryIntent.setAction(Intent.ACTION_PICK);
+                                galleryIntent.setType("image/*");
+                                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+                            } else if (item.getTitle().equals("Take a Selfie")) {
+                                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                if (takePictureIntent.resolveActivity(ProfileActivity.this.getPackageManager()) != null) {
+                                    startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+                                }
                             }
+                            Toast.makeText(ProfileActivity.this, "You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                            return true;
                         }
-                        Toast.makeText(
-                                ProfileActivity.this,
-                                "You Clicked : " + item.getTitle(),
-                                Toast.LENGTH_SHORT
-                        ).show();
-                        return true;
-                    }
-                });
+                    });
 
-                popup.show(); //showing popup menu
+                    popup.show(); //showing popup menu
+                }
 
 
             }
         });
 
+
+    }
+
+    void LoadUserInfo()
+    {
+        mDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                userInformation = dataSnapshot.getValue(UserInformation.class);
+                Picasso.with(getApplicationContext()).load(userInformation.getImage()).fit().into(profilephoto);
+                firstName.setText(userInformation.getFirstName());
+                lastName.setText(userInformation.getLastName());
+                Age.setText(userInformation.getAge());
+                firstname.setHint(userInformation.getFirstName());
+                lastname.setHint(userInformation.getLastName());
+                age.setHint(userInformation.getAge());
+
+
+
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK)
+        {
+
+            Picasso.with(getApplicationContext()).load(data.getData()).fit().into(profilephoto);
+            imageUri = data.getData();
+
+        }
+
+    }
 }
